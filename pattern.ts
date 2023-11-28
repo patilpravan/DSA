25d95fc68fca4a6e50a08e10e34fdf037f8fc317
Pravan <pravanpatil3@gmail.com>
2023-11-28 07:48:54 +0530

    new pattern

--- pattern.ts
+++ pattern.ts
@@ -83,4 +83,136 @@ function pyramid(n: number) {
     row = "";
   }
 }
-pyramid(5);
+// pyramid(5);
+
+/*
+
+*      *
+**    **
+***  ***
+********
+***  ***
+**    **
+*      *
+
+*/
+
+function xpattern(n: number) {
+  let rev = n;
+  let revBlank = 0;
+  for (let i = 0; i <= 2 * n - 1; i++) {
+    let row = "";
+    if (i <= n) {
+      for (let j = 1; j <= i; j++) {
+        row += "*";
+      }
+      for (let j = 1; j <= 2 * (n - i); j++) {
+        row += " ";
+      }
+      for (let j = 1; j <= i; j++) {
+        row += "*";
+      }
+    } else {
+      rev--;
+      revBlank++;
+      for (let j = 1; j <= rev; j++) {
+        row += "*";
+      }
+      for (let j = 1; j <= 2 * revBlank; j++) {
+        row += " ";
+      }
+      for (let j = 1; j <= rev; j++) {
+        row += "*";
+      }
+    }
+    console.log(row);
+    row = "";
+  }
+}
+xpattern(5);
+
+/*
+1------1      i - 2*(n-i) - i
+12----21
+123--321
+12344321
+*/
+
+function numPattern(n: number) {
+  for (let m = 1; m <= n; m++) {
+    let row = "";
+    for (let p = 1; p <= m; p++) {
+      row += p.toString();
+    }
+    for (let q = 1; q <= 2 * (n - m); q++) {
+      row += " ";
+    }
+    for (let p = m; p >= 1; p--) {
+      row += p.toString();
+    }
+    console.log(row);
+    row = "";
+  }
+}
+// numPattern(5);
+const alphabet = [
+  "0",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+/*
+---a---
+--aba--
+-abcba-
+abcdcba
+*/
+
+function alphaPAttern(n: number) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= n - i; j++) {
+      row += " ";
+    }
+    let rev = i;
+    for (let j = 1; j <= 2 * i - 1; j++) {
+      if (j <= i) {
+        row += alphabet[j];
+      } else {
+        rev--;
+        row += alphabet[rev];
+      }
+    }
+    for (let j = 1; j <= n - i; j++) {
+      row += " ";
+    }
+    console.log(row);
+    row = "";
+  }
+}
+
+// alphaPAttern(5);
